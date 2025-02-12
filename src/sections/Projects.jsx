5f455c06fbc9870f9f58e9354a6771fb492271c9
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-12 20:27:04 +0530

    next / previous button  improvements

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -25,19 +25,20 @@ export default function Projects() {
     <section className='c-space my-20'>
         <p className='head-text'>My Work</p>
         <div className='grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full' >
-            <div className='flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200'>
-                <div className='absolute top-0 right-0'>
+            <div className='flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200 justify-between'>
+                <div>
+                    <div className='absolute top-0 right-0'>
                     <img src={currentproject.spotlight} alt='spotlight' className='w-full h-96 object-cover rounded-xl' />
-                </div>
-                <div className='p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg' style={currentproject.logoStyle}>
+                    </div>
+                    <div className='p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg' style={currentproject.logoStyle}>
                     <img src={currentproject.logo} alt='logo' className='w-10 h-10 shadow-sm'/>
-                </div>   
-                <div className='flex flex-col gap-5 text-white-600 my-5'>
+                    </div>   
+                    <div className='flex flex-col gap-5 text-white-600 my-5'>
                     <p className='text-white text-2xl font-semibold animatedText'>{currentproject.title}</p>
                     <p className='animatedText'>{currentproject.desc}</p>
                     <p className='animatedText'>{currentproject.subdesc}</p>
-                </div>
-                <div className='flex items-center justify-between flex-wrap gap-5'>
+                    </div>
+                    <div className='flex items-center justify-between flex-wrap gap-5'>
                     <div className='flex items-center gap-3'>
                         {currentproject.tags.map((tag,i) => (<div key={i} className='tech-logo'>
                             <img src={tag.path} alt={tag.name} />
@@ -45,6 +46,7 @@ export default function Projects() {
                     </div>
                     <a className='flex items-center gap-2 cursor-pointer text-white-600' href={currentproject.href} target='_blank' rel='noreferrer'><p>Check live Site</p>
                     <img src='/assets/arrow-up.png' className='w-3 h-3' alt='arrow'/></a>
+                    </div>
                 </div>
                 <div className='flex justify-between items-center mt-7'>
                     <button className='arrow-btn' onClick={() => handlenavigation('previous')} >
